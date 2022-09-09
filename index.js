4d34490022b8cb05d4aed5db5c821d4312bc6dbb
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-09-08 22:25:23 -0500

    Update index.js
    Added additional director info

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Require modeles & packages
 const express = require("express");
 
 const fs = require("fs");
@@ -10,17 +11,29 @@ const path = require("path");
 
 const uuid = require("uuid");
 
+const bodyParser = require("body-parser");
+
+const methodOverride = require("method-override");
+
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
-
+//App use methods from modules
 app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(express.static("public"));
 
-const bodyParser = require("body-parser"),
-  methodOverride = require("method-override");
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(bodyParser.json());
+
+app.use(methodOverride());
+//Log any errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+//User Array
 let users = [
   {
     id: 1,
@@ -38,13 +51,18 @@ let users = [
     favoriteMovies: ["Gladiator", "Inception"],
   },
 ];
-
+//Movie Array
 const movies = [
   {
     title: "Fellowship of the Ring",
     description:
       "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
-    director: { name: "Peter Jackson", bio: "Some info about Peter Jackson" },
+    director: {
+      name: "Peter Jackson",
+      bio: "Some info about Peter Jackson",
+      born: 1961,
+      hometown: "Wellington, New Zealand",
+    },
     genre: "Action",
     releaseYear: "2001",
   },
@@ -52,7 +70,12 @@ const movies = [
     title: "The Two Towers",
     description:
       "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard.",
-    director: { name: "Peter Jackson" },
+    director: {
+      name: "Peter Jackson",
+      bio: "Some info about Peter Jackson",
+      born: 1961,
+      hometown: "Wellington, New Zealand",
+    },
     genre: "Action",
     releaseYear: "2002",
   },
@@ -60,7 +83,12 @@ const movies = [
     title: "The Return of the King",
     description:
       "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
-    director: { name: "Peter Jackson" },
+    director: {
+      name: "Peter Jackson",
+      bio: "Some info about Peter Jackson",
+      born: 1961,
+      hometown: "Wellington, New Zealand",
+    },
     genre: "Action",
     releaseYear: "2003",
   },
@@ -68,7 +96,12 @@ const movies = [
     title: "A New Hope",
     description:
       "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
-    director: { name: "George Lucas" },
+    director: {
+      name: "George Lucas",
+      bio: "Some info about George Lucas",
+      born: 1944,
+      hometown: "Modesto, CA",
+    },
     genre: "Action, Sci-fi",
     releaseYear: "1977",
   },
@@ -76,7 +109,12 @@ const movies = [
     title: "Empire Strikes Back",
     description:
       "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued across the galaxy by Darth Vader and bounty hunter Boba Fett.",
-    director: { name: "George Lucas" },
+    director: {
+      name: "George Lucas",
+      bio: "Some info about George Lucas",
+      born: 1944,
+      hometown: "Modesto, CA",
+    },
     genre: "Action, Sci-fi",
     releaseYear: "1980",
   },
@@ -84,7 +122,12 @@ const movies = [
     title: "Return of the Jedi",
     description:
       "After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy the second Death Star. Meanwhile, Luke struggles to help Darth Vader back from the dark side without falling into the Emperor's trap.",
-    director: { name: "George Lucas" },
+    director: {
+      name: "George Lucas",
+      bio: "Some info about George Lucas",
+      born: 1944,
+      hometown: "Modesto, CA",
+    },
     genre: "Action, Sci-fi",
     releaseYear: "1983",
   },
@@ -92,7 +135,12 @@ const movies = [
     title: "Gladiator",
     description:
       "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
-    director: { name: "Ridley Scott" },
+    director: {
+      name: "Ridley Scott",
+      bio: "Some info about Ridley Scott",
+      born: 1937,
+      hometown: "Durham, England",
+    },
     genre: "Action, History",
     releaseYear: "2000",
   },
@@ -100,7 +148,12 @@ const movies = [
     title: "Avengers",
     description:
       "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-    director: { name: "Joss Whedon" },
+    director: {
+      name: "Joss Whedon",
+      bio: "Some info about Joss Whedon",
+      born: 1964,
+      hometown: "New York City, NY",
+    },
     genre: "Action, Sci-fi, Sci-fi",
     releaseYear: "2012",
   },
@@ -108,7 +161,13 @@ const movies = [
     title: "Troy",
     description:
       "An adaptation of Homer's great epic, the film follows the assault on Troy by the united Greek forces and chronicles the fates of the men involved.",
-    director: { name: "Wolfgang Peterson" },
+    director: {
+      name: "Wolfgang Peterson",
+      bio: "Some info about Wolfgang Peterson",
+      born: 1941,
+      death: 2022,
+      hometown: "Los Angeles, CA",
+    },
     genre: "Action, History",
     releaseYear: "2004",
   },
@@ -116,7 +175,12 @@ const movies = [
     title: "Inception",
     description:
       "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.",
-    director: { name: "Christopher Nolan" },
+    director: {
+      name: "Christopher Nolan",
+      bio: "Some info about Christopher Nolan",
+      born: 1970,
+      hometown: "London, England",
+    },
     genre: "Action",
     releaseYear: "2010",
   },
@@ -228,17 +292,6 @@ app.delete("/users/:id", (req, res) => {
   }
 });
 
-//2.3
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(bodyParser.json());
-
-app.use(methodOverride());
-//Log any errors
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
 // Server running pn port 8080
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
