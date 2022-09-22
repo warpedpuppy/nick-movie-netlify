c0667b7b7bcf160b68896c52be9760cf5ae45283
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-09-22 15:46:33 -0500

    Create auth.js
    created auth.js to use jwt tokens

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwtSecret = "your_jwt_secret"; // Must be same key used in JWTStrategy
+
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
+
+require("./passport"); //Local passport file
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // Username encoding in the JWT
+    expiresIn: "7d", //Token will expire in seven days
+    algorithm: "HS256", // Algorithm used to 'sign' or encode the values of JWT
+  });
+};
+
+/* POST login*/
+module.exports = (router) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: "Something is not right",
+          user: user,
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
